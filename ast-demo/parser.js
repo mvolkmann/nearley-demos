c109afb13dc8926b68c783bdd2a379ff8650a429
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-02-27 19:11:00 -0600

    progress

--- ast-demo/parser.js
+++ ast-demo/parser.js
@@ -4,10 +4,19 @@ import grammar from "./arithmetic-ast.ts";
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 const input = "2 * 3 + (5 + 1) / 2 - 4"; // expect 5
 
-function evaluateAstNode(node) {
+function evaluateAST(node) {
+  switch (node.type) {
+    case "binary operation":
+      return evaluateBinaryOperation(node);
+    default:
+      throw new Error(`unsupported AST node type "${node.type}"`);
+  }
+}
+
+function evaluateBinaryOperation(node) {
   let { left, operator, right } = node;
-  if (typeof left !== "number") left = evaluateAstNode(left);
-  if (typeof right !== "number") right = evaluateAstNode(right);
+  if (typeof left !== "number") left = evaluateAST(left);
+  if (typeof right !== "number") right = evaluateAST(right);
   switch (operator) {
     case "+":
       return left + right;
@@ -23,7 +32,7 @@ function evaluateAstNode(node) {
 try {
   parser.feed(input);
   console.log(parser.results);
-  console.log(evaluateAstNode(parser.results[0]));
+  console.log(evaluateAST(parser.results[0]));
 } catch (e) {
   console.error(e.toString());
 }
